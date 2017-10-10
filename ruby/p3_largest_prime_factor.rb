86d4b3bce7a24d5b7f3bae9abb75994377a74bc9
harrisjm <hjmotiwala@utexas.edu>
2017-10-10 15:20:37 -0500

    planned out problem 3 in ruby

---
+++ ruby/p3_largest_prime_factor.rb
@@ -0,0 +1,17 @@
+# Problem 3
+#
+# ----------------------------------------------------------------------
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+#
+# ----------------------------------------------------------------------
+
+factors = []
+
+def factorize
+  (2...num).each do |poss_fac|
+    if (num % poss_fac === 0)
+  end
+end
